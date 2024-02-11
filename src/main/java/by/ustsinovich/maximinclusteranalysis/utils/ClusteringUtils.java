b2661d951c171cdb3fceb7b98f522f9e5407be09
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-02-11 22:52:35 +0300

    fix algorithm

--- src/main/java/by/ustsinovich/maximinclusteranalysis/utils/ClusteringUtils.java
+++ src/main/java/by/ustsinovich/maximinclusteranalysis/utils/ClusteringUtils.java
@@ -10,77 +10,38 @@ public class ClusteringUtils {
         List<Cluster> clusters = new ArrayList<>();
         List<Point> remainingPoints = new ArrayList<>(points);
 
-        initializeFirstCluster(clusters, remainingPoints);
-
-        while (!remainingPoints.isEmpty()) {
-            var pointToNearestClusterDistance = calculateDistancesToNearestCluster(remainingPoints, clusters);
-
-            Point farthestPoint = findFarthestPoint(pointToNearestClusterDistance);
-
-            double averageInterClusterDistance = calculateAverageInterClusterDistance(clusters);
-
-            createNewCluster(remainingPoints, clusters, pointToNearestClusterDistance,
-                    farthestPoint, averageInterClusterDistance);
-        }
-
-        assignRemainingPointsToNearestCluster(remainingPoints, clusters);
-
-        return clusters;
-    }
-
-    private static void initializeFirstCluster(List<Cluster> clusters, List<Point> remainingPoints) {
         Point initialPoint = remainingPoints.remove(new Random().nextInt(remainingPoints.size()));
         clusters.add(new Cluster(initialPoint));
-    }
 
-    private static Map<Point, Double> calculateDistancesToNearestCluster(List<Point> remainingPoints, List<Cluster> clusters) {
-        Map<Point, Double> pointToNearestClusterDistance = new HashMap<>();
-        for (Point point : remainingPoints) {
-            double minDistance = Double.MAX_VALUE;
-            for (Cluster cluster : clusters) {
-                double distance = PointsUtils.calculateDistance(point, cluster.getCentroid());
-                minDistance = Math.min(minDistance, distance);
+        while (!remainingPoints.isEmpty()) {
+            Map<Point, Double> pointToNearestClusterDistance = new HashMap<>();
+            for (Point point : remainingPoints) {
+                double minDistance = Double.MAX_VALUE;
+                for (Cluster cluster : clusters) {
+                    double distance = PointsUtils.calculateDistance(point, cluster.getCentroid());
+                    minDistance = Math.min(minDistance, distance);
+                }
+                pointToNearestClusterDistance.put(point, minDistance);
             }
-            pointToNearestClusterDistance.put(point, minDistance);
-        }
-        return pointToNearestClusterDistance;
-    }
 
-    private static Point findFarthestPoint(Map<Point, Double> pointToNearestClusterDistance) {
-        Point farthestPoint = null;
-        double maxDistance = Double.MIN_VALUE;
-        for (Map.Entry<Point, Double> entry : pointToNearestClusterDistance.entrySet()) {
-            if (entry.getValue() > maxDistance) {
-                maxDistance = entry.getValue();
-                farthestPoint = entry.getKey();
+            Point farthestPoint = null;
+            double maxDistance = Double.MIN_VALUE;
+            for (Map.Entry<Point, Double> entry : pointToNearestClusterDistance.entrySet()) {
+                if (entry.getValue() > maxDistance) {
+                    maxDistance = entry.getValue();
+                    farthestPoint = entry.getKey();
+                }
             }
-        }
-        return farthestPoint;
-    }
 
-    private static double calculateAverageInterClusterDistance(List<Cluster> clusters) {
-        double totalDistance = 0;
-        int count = 0;
-        for (int i = 0; i < clusters.size(); i++) {
-            for (int j = i + 1; j < clusters.size(); j++) {
-                totalDistance += PointsUtils.calculateDistance(clusters.get(i).getCentroid(), clusters.get(j).getCentroid());
-                count++;
+            double averageInterClusterDistance = calculateAverageInterClusterDistance(clusters);
+            if (maxDistance > averageInterClusterDistance / 2) {
+                remainingPoints.remove(farthestPoint);
+                clusters.add(new Cluster(farthestPoint));
+            } else {
+                break;
             }
         }
-        return count == 0 ? 0 : totalDistance / count;
-    }
 
-    private static void createNewCluster(List<Point> remainingPoints, List<Cluster> clusters,
-                                         Map<Point, Double> pointToNearestClusterDistance, Point farthestPoint,
-                                         double averageInterClusterDistance) {
-        double maxDistance = Collections.max(pointToNearestClusterDistance.values());
-        if (maxDistance > averageInterClusterDistance / 2) {
-            remainingPoints.remove(farthestPoint);
-            clusters.add(new Cluster(farthestPoint));
-        }
-    }
-
-    private static void assignRemainingPointsToNearestCluster(List<Point> remainingPoints, List<Cluster> clusters) {
         for (Point point : remainingPoints) {
             Cluster nearestCluster = null;
             double minDistance = Double.MAX_VALUE;
@@ -95,6 +56,23 @@ public class ClusteringUtils {
                 nearestCluster.addPoint(point);
             }
         }
+
+        return clusters;
+    }
+
+    private static double calculateAverageInterClusterDistance(List<Cluster> clusters) {
+        double totalDistance = 0;
+        int count = 0;
+        for (int i = 0; i < clusters.size(); i++) {
+            for (int j = i + 1; j < clusters.size(); j++) {
+                totalDistance += PointsUtils.calculateDistance(
+                        clusters.get(i).getCentroid(),
+                        clusters.get(j).getCentroid()
+                );
+                count++;
+            }
+        }
+        return count == 0 ? 0 : totalDistance / count;
     }
 }
 
